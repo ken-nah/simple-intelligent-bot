7f053faf2c4e27af2624647f1954ff75bf8f7bb4
ken-nah <Kennyadduz@gmail.com>
2019-03-15 13:08:50 +0300

    Completed developing the app landing page

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ const path = require("path");
 
 const express = require("express");
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 const APIAI_TOKEN = process.env.APIAI_TOKEN;
 const APIAI_SESSION_ID = process.env.APIAI_SESSION_ID;
 
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require("./routes/authRoutes");
 
 app.use(express.static(path.join(__dirname, "public"))); //js,css
 app.use(express.static(path.join(__dirname, "views"))); //html
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.use('/auth',authRoutes);
-
+app.use("/auth", authRoutes);
 
 const port = process.env.PORT || 3000;
 
@@ -23,37 +22,35 @@ const server = app.listen(port, () =>
   console.log(`Server Listening on port ${port}...`)
 );
 
-const io = require('socket.io')(server);
-io.on('connection', function (socket) {
-  console.log('a user connected');
+const io = require("socket.io")(server);
+io.on("connection", function(socket) {
+  console.log("a user connected");
 });
 
-const apiai = require('apiai')(APIAI_TOKEN);
+const apiai = require("apiai")(APIAI_TOKEN);
 
-// Web UI
-app.get('/', (req, res) => {
-  res.sendFile('index.html');
+// Homepage
+app.get("/", (req, res) => {
+  return res.sendFile("index.html");
 });
 
-io.on('connection', function (socket) {
-  socket.on('chat message', (text) => {
-
+io.on("connection", function(socket) {
+  socket.on("chat message", text => {
     // Get a reply from API.ai
 
     let apiaiReq = apiai.textRequest(text, {
       sessionId: APIAI_SESSION_ID
     });
 
-    apiaiReq.on('response', (response) => {
+    apiaiReq.on("response", response => {
       let aiText = response.result.fulfillment.speech;
-      socket.emit('bot reply', aiText);
+      socket.emit("bot reply", aiText);
     });
 
-    apiaiReq.on('error', (error) => {
+    apiaiReq.on("error", error => {
       console.log(error);
     });
 
     apiaiReq.end();
-
   });
 });
